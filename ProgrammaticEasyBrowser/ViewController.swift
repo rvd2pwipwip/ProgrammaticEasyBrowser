cd419f9abb746eb5d4d32b200fd21df0d323ef1a
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-15 12:52:06 -0500

    finished part 4, Monitoring page loads: UIToolbar and UIProgressView, ready for part 5 refactoring

--- ProgrammaticEasyBrowser/ViewController.swift
+++ ProgrammaticEasyBrowser/ViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate { // promise to conform to WKNavigationDelegate protocol
     
     private var webView: WKWebView! // need to be stored as a property to be referenced later
+    private var progressView: UIProgressView!
 
     override func loadView() {
         super.loadView()
@@ -26,9 +27,31 @@ class ViewController: UIViewController, WKNavigationDelegate { // promise to con
         title = "Easy Web Browser" // our view has a navigationController with a title property...
 //        navigationItem.rightBarButtonItem = rightBarButtonItem // ...and a navigationItem
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped)) // action works here...
+        
+        progressView = UIProgressView(progressViewStyle: .default) // create progressView with defailt style
+        progressView.sizeToFit() // set progressView layout size so that it fits its contents fully
+        let progressButton = UIBarButtonItem(customView: progressView) // wrap progressView as custom view item to be used in toolbarItems' array
+        
+        // prepare KVO (key-value observing) to ask iOS to tell us when property X of object Y changes
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil) //1. observer, 2. what 3. which value 4. context
+        // Warning: in more complex applications, all calls to addObserver() should be matched with a call to removeObserver()
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        toolbarItems = [progressButton, spacer, refresh] // items of the toolbar, displayed in array order
+        navigationController?.isToolbarHidden = false // need to manually show the toolbar
+        
         let url = URL(string: "https://www.stingray.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    // Once you have registered as an observer using KVO, you must implement a method called observeValue()
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
     @objc func openTapped() {
